2b523d30e95faf910577fc245896695b6da5c742
nhtri <huutri26697@gmail.com>
2024-11-28 15:00:38 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "selector",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    import('@tailwindcss/forms'),
+  ],
+}
+
